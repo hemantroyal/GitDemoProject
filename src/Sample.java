3868d94c08f841f61e01970ba32744a8c518fd09
hemanth royal <hemanth.b.royal@gmail.com>
2021-04-23 20:35:30 +0200

    First Commit..

---
+++ src/Sample.java
@@ -0,0 +1,15 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("eat something he is hungry");
+		else {
+			System.out.println("he is not hungry");
+		}
+
+	}
+
+}
